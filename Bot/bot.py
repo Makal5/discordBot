2717779782eb2610e3f16e56d1a0f1e07a9d98e1
Makal5 <marks.cimermanis@gmail.com>
2022-12-05 03:04:23 +0200

    Added some comments

--- Bot/bot.py
+++ Bot/bot.py
@@ -11,6 +11,7 @@ LOGGER = logging.getLogger(__name__)
 async def send_message(message, user_message, isPrivate):
     """Bot set up"""
     try:
+        # logger if is private channel or community channel
         resp_channel = 'channel'
         if isPrivate:
             resp_channel = 'private'
@@ -30,13 +31,17 @@ def run_discord_bot():
 
     @client.event
     async def on_ready():
+        """On ready log to info"""
         LOGGER.info(f'{client.user} is up and running')
 
     @client.event
     async def on_message(message):
+        """Message working"""
+        # Check if message author == to bot
         if message.author == client.user:
             return
 
+        # Check if message does not contain bad words
         if message.content != "ass" or message.content != "fuck":
             pass
         else:
@@ -46,8 +51,10 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
+        # logger debug with username, channel and said message
         LOGGER.debug(f'{username} in {channel} said: "{user_message}"')
 
+        # check if user message starts with ?
         if user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, isPrivate=True)
